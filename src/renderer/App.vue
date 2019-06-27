9c537c35ecbebf00aa7e26917c83aeb68839fbcc
Luis Portillo <luisxxor@gmail.com>
2019-06-26 23:44:08 -0400

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'esteno_help_client'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
